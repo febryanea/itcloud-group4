1d151b15be4a7f2c20f2a8518bbd3c3c4eb1fb43
febryanea <81205600+febryanea@users.noreply.github.com>
2022-12-07 01:37:21 +0700

    Update test.js

--- scripts/test.js
+++ scripts/test.js
@@ -3,5 +3,6 @@ import { sleep } from 'k6';
 
 export default function () {
     const res = http.get('https://group4-static.s3.ap-southeast-1.amazonaws.com/index.html');
+    console.log('URL TESTED : https://group4-static.s3.ap-southeast-1.amazonaws.com/index.html');
     sleep(1);
 }
