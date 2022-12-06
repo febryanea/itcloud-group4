95c260e97601c7aa0e186287582b67dbba7ac77f
febryanea <81205600+febryanea@users.noreply.github.com>
2022-12-07 00:54:18 +0700

    Update test.js

--- scripts/test.js
+++ scripts/test.js
@@ -2,6 +2,6 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export default function () {
-    const res = http.get('https://test.k6.io');
+    const res = http.get('https://group4-static.s3.ap-southeast-1.amazonaws.com/index.html');
     sleep(1);
 }
