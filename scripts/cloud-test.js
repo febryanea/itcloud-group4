201e45d90d96825a774df92d642b041cf2920049
patriamd <119581821+patriamd@users.noreply.github.com>
2022-12-08 13:30:14 +0700

    Update cloud-test.js

--- scripts/cloud-test.js
+++ scripts/cloud-test.js
@@ -17,6 +17,5 @@ export const options = {
 
 export default function () {
     const res = http.get('https://group4-static.s3.ap-southeast-1.amazonaws.com/index.html');
-    console.log('URL TESTED : https://group4-static.s3.ap-southeast-1.amazonaws.com/index.html');
     sleep(1);
 }
